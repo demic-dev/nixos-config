0d1441b9bdfcc1625e85ddb10a1a057088b65e4e
Michele De Cillis <decillismicheledeveloper@gmail.com>
2026-04-28 13:34:22 +0200

    updated rebuild alias

--- hosts/bach/home.nix
+++ hosts/bach/home.nix
@@ -15,7 +15,7 @@ in
   programs.zsh = {
     shellAliases = {
       # Until when I don't solve how to load sensitive variables in a private way, I will use this ugly approach.
-      update = "source /home/.config/sensitive.sh && sudo -E nixos-rebuild switch --flake /home/.config/#bach --impure";
+      update = "source ~/nixos/sensitive.sh && sudo -E nixos-rebuild switch --flake ~/nixos/#bach --impure";
     };
   };
 }
